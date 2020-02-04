aa2fb4cc9638ef11639de6b17226d640569da174
cansuatalay <cansuaatalay7@gmail.com>
2020-02-04 22:10:06 +0300

    Update OrderController.java

--- ShoppingCart-master/src/main/java/com/controller/OrderController.java
+++ ShoppingCart-master/src/main/java/com/controller/OrderController.java
@@ -26,14 +26,13 @@ public class OrderController {
 		CustomerOrder customerOrder = new CustomerOrder();
 
 		Cart cart = cartService.getCartByCartId(cartId);
-		// Update CartId for customerOrder - set CartId
+		
 		customerOrder.setCart(cart);
 
 		Customer customer = cart.getCustomer();
 
 		customerOrder.setCustomer(customer);
-		// Set customerid
-		// Set ShippingAddressId
+	
 		customerOrder.setShippingAddress(customer.getShippingAddress());
 
 		customerOrder.setBillingAddress(customer.getBillingAddress());
