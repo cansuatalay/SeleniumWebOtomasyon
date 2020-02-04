f46f685acdc5cb8d739c0ad1268c2fab9eff2fea
cansuatalay <cansuaatalay7@gmail.com>
2020-02-04 22:14:18 +0300

    Update ProductDaoImpl.java

--- ShoppingCart-master/src/main/java/com/dao/ProductDaoImpl.java
+++ ShoppingCart-master/src/main/java/com/dao/ProductDaoImpl.java
@@ -12,16 +12,12 @@ import com.model.Product;
 @Repository(value = "productDao")
 public class ProductDaoImpl implements ProductDao {
 
-	// this class is wired with the sessionFactory to do some operation in the
-	// database
+	
 
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	// this will create one sessionFactory for this class
-	// there is only one sessionFactory should be created for the applications
-	// we can create multiple sessions for a sessionFactory
-	// each session can do some functions
+	
 
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
@@ -32,24 +28,23 @@ public class ProductDaoImpl implements ProductDao {
 	}
 
 	public List<Product> getAllProducts() {
-		// Reading the records from the table
+		
 		Session session = sessionFactory.openSession();
-		// List<Product> products = session.createQuery("from Product").list();
+		
 		List<Product> products = session.createCriteria(Product.class).list();
 		System.out.println("----- List of Products-----");
 		System.out.println(products);
-		// session.flush is used for clear cache in the session
+		
 		session.flush();
-		// it will close the particular session after completing the process
+		
 		session.close();
 		return products;
 	}
 
 	public Product getProductById(String productId) {
 
-		// Reading the records from the table
 		Session session = sessionFactory.openSession();
-		// select * from Product where isbn=i
+		
 		Product product = (Product) session.get(Product.class, productId);
 		session.close();
 		return product;
