d7323200ad9a13ba3af4131f84e454ad0c391e1a
cansuatalay <cansuaatalay7@gmail.com>
2020-02-04 22:15:06 +0300

    Update UserDaoImpl.java

--- ShoppingCart-master/src/main/java/com/dao/UserDaoImpl.java
+++ ShoppingCart-master/src/main/java/com/dao/UserDaoImpl.java
@@ -39,11 +39,9 @@ public class UserDaoImpl implements UserDao {
 	}
 
 	public User getUserById(String userId) {
-		// Reading the records from the table
+		
 		Session session = sessionFactory.openSession();
-		// select * from Product where isbn=i
-		// if we call get method,Record doesnot exist it will return null
-		// if we call load, if the record doesnt exist it will throw exception
+		
 		User user = (User) session.get(User.class, userId);
 		session.close();
 		return user;
